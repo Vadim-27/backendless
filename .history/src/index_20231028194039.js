f25305254000812ff1e3de8aa679e46d792f3001
Vadim-27 <vadim.bulanyi@gmail.com>
2023-10-28 19:49:33 +0200

    fix path

--- .history/src/index_20231028194039.js
+++ .history/src/index_20231028194039.js
@@ -6,7 +6,7 @@ import './index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/backendless">
+    <BrowserRouter basename="/tabs/dummyTable">
       <App />
     </BrowserRouter>
   </React.StrictMode>
